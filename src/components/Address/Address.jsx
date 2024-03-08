319aec28c105d23fdec2bde6163957d2407c5ade
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-03-08 15:14:27 +0700

    feat(address): update address component

---
+++ src/components/Address/Address.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Address = () => {
+    return (
+        <div>
+            <header>
+                <div className="address-name"></div>
+                <div className="location-address"></div>
+            </header>
+            <main>
+                <div className="contact-address"></div>
+                <div className="social-media"></div>
+            </main>
+            <footer>
+                <div className="beranda"></div>
+                <div className="profile"></div>
+                <div className="referensi"></div>
+                <div className="referensi"></div>
+                <div className="referensi"></div>
+            </footer>
+        </div>
+    );
+};
+
+export default Address;
