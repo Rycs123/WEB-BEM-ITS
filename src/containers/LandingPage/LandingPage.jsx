ce79c38df38958a8ac6d24c2f13490fcb0dc2b48
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-03-08 14:51:37 +0700

    feat: add LandingPage.jsx and LandingPage.js

---
+++ src/containers/LandingPage/LandingPage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Header from "../../components/Header/Header";
+import BlueLogo from "../../assets/img/BlueLogo.png";
+import LandingPageSelaras from "../../assets/img/LandingPage.svg";
+import "./style.css";
+import Footer from "../../components/Footer/Footer";
+
+const LandingPage = (props) => {
+    return (
+        <div>
+            <Header />
+            <main>
+                <div className="mainLandingPage"></div>
+                <div className="secondLandingPage"></div>
+                <div className="fastLinkLandingPage"></div>
+                <div className="newsBemLandingPage"></div>
+                <div className="progressLandingPage"></div>
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
+LandingPage.propTypes = {};
+
+export default LandingPage;
