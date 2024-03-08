20c7eb1fd39eef7aec63212f29ff0154aaab4eff
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-03-08 15:10:32 +0700

    feat(Selaras): add Selaras component

---
+++ src/components/Selaras/Selaras.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+const Selaras = () => {
+    return (
+        <div>
+            <Header />
+            <main>
+                <div className="vision"></div>
+            </main>
+            <section>
+                <div className="member"></div>
+            </section>
+            <Footer />
+        </div>
+    );
+};
+
+export default Selaras;
