b37da360d4b2831faaead3f633044441bfada439
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-03-08 15:10:07 +0700

    feat(sop): add sop component

---
+++ src/components/Sop/Sop.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const Sop = () => {
+    return (
+        <div>
+            <Header />
+            <main>
+                <div className="sop"></div>
+                <div className="contact"></div>
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
+export default Sop;
